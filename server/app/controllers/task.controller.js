f6625ca954bc307ba58663e5e4a2ade3e5004f62
yns-jns <younessaitmbarek97@gmail.com>
2024-01-30 14:57:05 +0100

    Deleted controllers and routes

--- server/app/controllers/task.controller.js
+++
@@ -1,7 +0,0 @@
-const db = require("../models");
-
-// const TaskModel = db.tasks;
-
-exports.getTest = (req, res) => {
-    res.json({ data: "Data from Server!! Test!!" });
-};
--- server/app/routes/task.routes.js
+++ server/app/routes/task.routes.js
@@ -3,8 +3,6 @@ const taskController = require("../controllers/task.controller");
 
 const router = express.Router();
 
-// Testing:
-router.get("/", taskController.getTest);
 
 // Exporting router:
 module.exports = router;
